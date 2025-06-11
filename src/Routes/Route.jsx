b143ade4bdd9c833050673f696d768fa5f9e2686
Rikon07 <mh13mahedi@gmail.com>
2025-06-11 10:15:43 +0600

    Initiated the Repo

---
+++ src/Routes/Route.jsx
@@ -0,0 +1,51 @@
+import { createBrowserRouter } from "react-router";
+import Root from "../Layouts/Root";
+import Home from "../Layouts/Home";
+import Login from "../Pages/Login";
+import Register from "../Pages/Register";
+import Error from "../Pages/Error";
+import ForgetPass from "../Pages/ForgetPass";
+import Profile from "../Pages/Profile";
+import Private from "../Provider/Private";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: Root,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        // loader: () => fetch('https://ryk-hub-freelance-server.vercel.app/tasks'),
+        Component: Home,
+      },
+      {
+        path: "/login",
+        Component: Login,
+      },
+      {
+        path: "/register",
+        Component: Register,
+      },
+      {
+        path: "/profile",
+        Component: () => (
+          <Private>
+            <Profile />
+          </Private>
+        ),
+      },
+      {
+        path: '/forget-password',
+        Component: ForgetPass,
+      },
+      {
+        path: '*',
+        Component: Error,
+      },
+    ],
+    
+  },
+]);
+
+export default router;
